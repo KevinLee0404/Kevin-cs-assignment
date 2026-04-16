e72776c8276badfdb0c38e44da2bc27d56be9327
Kevin <seungchan110404@gmail.com>
2026-04-16 13:03:39 +0800

    This is my fourth section among the 8 sections, honestly I didn't understand the question initially.

---
+++ codes/Percentage.py
@@ -0,0 +1,4 @@
+"""CodeHS Python Practice Basics — question 4. Add the official exercise title from CodeHS."""
+Write a function that takes in a number and a percentage and returns the percentage of that number.
+def percentage(num, percent):
+    return num*(percent/100)
