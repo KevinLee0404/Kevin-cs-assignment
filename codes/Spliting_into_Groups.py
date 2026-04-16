d436f10c155c9269e4bc080542df5990ffe37c7b
Kevin <seungchan110404@gmail.com>
2026-04-16 12:53:40 +0800

    This is my second section among the 8 sections, I learned floor division(//) and devide number of people with groups to get even split groups

---
+++ codes/Spliting_into_Groups.py
@@ -0,0 +1,4 @@
+"""CodeHS Python Practice Basics — question 2. Add the official exercise title from CodeHS."""
+Write a function that takes in a number of people and a number of groups. The people will be split up into the groups evenly (there might be left over people). Return the number of people in each group.
+def split_groups(num_people,num_groups):
+    return num_people // num_groups
