832330f1baeed4825ad147ff95ebdc437e73e14c
Kevin <seungchan110404@gmail.com>
2026-04-16 12:47:16 +0800

    This is my first section among the 8 assignments, I add two random number together to get the sum

--- codes/sum.py
+++ codes/sum.py
@@ -1,1 +1,4 @@
 """CodeHS Python Practice Basics — question 1. Add the official exercise title from CodeHS."""
+Write a function that takes in two numbers and returns the sum of those two numbers.
+def sum(num1,num2):
+    return num1+num2
